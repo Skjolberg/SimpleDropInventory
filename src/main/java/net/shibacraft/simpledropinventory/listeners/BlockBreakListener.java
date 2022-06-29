3f70bf05f7cd24d6346f7c1fcc1ffafdc6ee5ec2
SKJOL <danielvazquezmunoz@gmail.com>
2022-06-29 22:25:27 +0200

    Update

--- src/main/java/net/shibacraft/simpledropinventory/listeners/BlockBreakListener.java
+++ src/main/java/net/shibacraft/simpledropinventory/listeners/BlockBreakListener.java
@@ -1,50 +1,43 @@
 package net.shibacraft.simpledropinventory.listeners;
 
-import lombok.Getter;
-import net.shibacraft.simpledropinventory.SimpleDropInventory;
-import net.shibacraft.simpledropinventory.module.EventsModule;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.block.Block;
+import de.leonhard.storage.Yaml;
+import net.shibacraft.simpledropinventory.commands.MainCommand;
+import net.shibacraft.simpledropinventory.files.FileManager;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.inventory.ItemStack;
 
-import java.util.*;
+import java.util.Set;
+import java.util.UUID;
 
 public class BlockBreakListener implements Listener {
 
-    @Getter
-    public static final Set<UUID> drop = EventsModule.getDrop();
+    private final Set<UUID> drop = MainCommand.getDrop();
+    private Yaml config = FileManager.getFilesYaml().get("Config");
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void blockDropItem(BlockBreakEvent event) {
+        String world = event.getPlayer().getWorld().getName();
+        if(UtilsListener.isWorldDisabled(world)) return;
+
         Player p = event.getPlayer();
 
-        if(drop.contains(p.getUniqueId())){
-            Block b = event.getBlock();
-            Location blockLocation = b.getLocation();
-            Collection<ItemStack> items = b.getDrops();
-            for (ItemStack item : items) {
-                if (p.getInventory().firstEmpty() != -1) {
-                    p.getInventory().addItem(item);
-                } else {
-                    HashMap<Integer, ItemStack> res = p.getInventory().addItem(item);
-                    if (!res.isEmpty()) p.getWorld().dropItemNaturally(blockLocation, res.get(0));
-                }
+        if (drop.contains(p.getUniqueId())) {
+            int exp = event.getExpToDrop();
 
-            }
-            if (SimpleDropInventory.getVERSION() > 8) {
-                event.setDropItems(false);
-            } else {
-                b.setType(Material.AIR);
+            if (config.getBoolean("Collect-Experience")) {
+                event.getPlayer().giveExp(exp);
+                event.setExpToDrop(0);
             }
 
         }
 
+    }
+
+    public void reload() {
+        config = FileManager.getFilesYaml().get("Config");
     }
 
 }
---
+++ src/main/java/net/shibacraft/simpledropinventory/listeners/LegacyBlockBreakListener.java
@@ -0,0 +1,54 @@
+package net.shibacraft.simpledropinventory.listeners;
+
+import lombok.Getter;
+import net.shibacraft.simpledropinventory.SimpleDropInventory;
+import net.shibacraft.simpledropinventory.commands.MainCommand;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.*;
+
+public class LegacyBlockBreakListener implements Listener {
+
+    @Getter
+    public static final Set<UUID> drop = MainCommand.getDrop();
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void blockDropItem(BlockBreakEvent event) {
+        String world = event.getPlayer().getWorld().getName();
+        if(UtilsListener.isWorldDisabled(world)) return;
+
+        Player p = event.getPlayer();
+
+        if (drop.contains(p.getUniqueId())) {
+            Block b = event.getBlock();
+            Location blockLocation = b.getLocation();
+            Collection<ItemStack> items = b.getDrops();
+            for (ItemStack item : items) {
+                if (p.getInventory().firstEmpty() != -1) {
+                    p.getInventory().addItem(item);
+                } else {
+                    HashMap<Integer, ItemStack> res = p.getInventory().addItem(item);
+                    if (!res.isEmpty()) p.getWorld().dropItemNaturally(blockLocation, res.get(0));
+                }
+
+            }
+
+            if (SimpleDropInventory.getVERSION() > 8) {
+                event.setDropItems(false);
+            } else {
+                b.setType(Material.AIR);
+            }
+
+        }
+
+    }
+
+}
